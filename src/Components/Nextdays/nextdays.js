f06b59e3a83b4af4ffb4664d6d8fdb0cd3eef0b6
Pramod <pramodmithyantha@gmail.com>
2019-07-01 11:53:24 +0530

    Addition CSS - 1

--- src/Components/Nextdays/nextdays.js
+++ src/Components/Nextdays/nextdays.js
@@ -6,12 +6,15 @@ function Nextdays(props) {
     return props.nxt.map((res) => {
         return (
             <div className="block" key={res.day}>
-                <p><label> Day :</label>{res.day}</p>
-                <p><label> Location :</label>{res.location}</p>
-                <p><label> Current :</label>{res.current}</p>
-                <p><label> Maximum_temperature :</label> {res.maximum_temp}</p>
-                <p><label> Minimum_temperature :</label>{res.minimum_temp}</p>
-                <p><Display current={res.current} /></p>
+                <p style={{fontSize:'30px',textAlign:'center'}}>{res.day}</p>
+                <p>Location : {res.location}</p>
+                <p><label> Current : </label>{res.current}</p>
+                <p><label> Maximum_temperature : </label> {res.maximum_temp}</p>
+                <p><label> Minimum_temperature : </label>{res.minimum_temp}</p>
+                <div style={{textAlign:'center'}}>
+                <Display current={res.current} />
+                </div>
+                
             </div>
         )
     });
--- src/Components/weather.js
+++ src/Components/weather.js
@@ -6,10 +6,13 @@ import Nextdays from './Nextdays/nextdays';
 function Weather() {
     return (
         <div>
-            <h2 style={{textAlign:'center'}}>Weather report</h2>
-            <Today today={weather} /> <br /><br /><br /><br />
             <div>
-            <h3>Upcoming Days</h3>
+            <h2 style={{textAlign:'center'}}>Weather report</h2>
+            </div>
+            
+            <Today today={weather} /> <br /><br /><br /><br /> <hr />
+            <div>
+            <h3 style={{textAlign:'center'}}>Upcoming Days</h3>
                 <Nextdays nxt={weather.next} />
             </div>
         </div>
